5931f75fce6ba6a68e7548285ee87e899030a1eb
Stanimir Stoev <stoevs@gmail.com>
2016-11-15 23:44:50 +0200

    Started setting up VS Code and ESLint

---
+++ Overview/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0,
+        "eqeqeq": 2
+    }
+};
---
+++ Overview/app.js
@@ -0,0 +1,7 @@
+
+
+let var1 = 6;
+
+console.log('jhjhjhjhjhjjh');
+console.log('object');
+console.log(var1);
